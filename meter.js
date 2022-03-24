91c4f0cd04d339168771c7489e87f77c022817e9
AladortheWizard <64230805+AladortheWizard@users.noreply.github.com>
2022-03-24 08:38:09 -0600

    Update and rename gravity.js to meter.js

---
+++ meter.js
@@ -0,0 +1,11 @@
+javascript: (
+  function() {
+    document.write("hi"); 
+    var x = document.createElement("METER");
+    x.setAttribute("min", "0");
+    x.setAttribute("max", "100"); 
+    x.setAttribute("value", "65");
+    document.body.appendChild(x);
+  }
+)
+();
